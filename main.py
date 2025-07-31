f5ff94ef8178a0545fb185cd65fc6bdebb0cdbeb
MinadukiSekina <taroufujimoto075@gmail.com>
2025-07-31 07:21:55 +0000

    feat: #1 implement environment variable loading and processing

--- main.py
+++ main.py
@@ -1,9 +1,70 @@
-from my_package import hello
+import bs4
+from langchain import hub
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_core.documents import Document
+from langchain_core.vectorstores import InMemoryVectorStore
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langgraph.graph import START, StateGraph
+from typing_extensions import TypedDict
+
+from src.my_package.environment.env_loader import load_env
+
+# 環境変数の読み込み
+load_env()
+
+# Load and chunk contents of the blog
+# Only keep post title, headers, and content from the full HTML.
+loader = WebBaseLoader(
+    web_paths=("https://lilianweng.github.io/posts/2023-06-23-agent/",),
+    bs_kwargs=dict(
+        parse_only=bs4.SoupStrainer(class_=("post-content", "post-title", "post-header"))
+    ),
+)
+docs = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+all_splits = text_splitter.split_documents(docs)
+
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+vector_store = InMemoryVectorStore(embeddings)
+
+# Initialize the language model
+llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+# Index chunks
+_ = vector_store.add_documents(documents=all_splits)
+
+# Define prompt for question-answering
+# N.B. for non-US LangSmith endpoints, you may need to specify
+# api_url="https://api.smith.langchain.com" in hub.pull.
+prompt = hub.pull("rlm/rag-prompt")
 
 
-def main() -> None:
-    print(hello())
+# Define state for application
+class State(TypedDict):
+    question: str
+    context: list[Document]
+    answer: str
 
 
-if __name__ == "__main__":
-    main()
+# Define application steps
+def retrieve(state: State) -> dict[str, list[Document]]:
+    retrieved_docs = vector_store.similarity_search(state["question"])
+    return {"context": retrieved_docs}
+
+
+def generate(state: State) -> dict[str, str | list[str | dict]]:
+    docs_content = "\n\n".join(doc.page_content for doc in state["context"])
+    messages = prompt.invoke({"question": state["question"], "context": docs_content})
+    response = llm.invoke(messages)
+    return {"answer": response.content}
+
+
+# Compile application and test
+graph_builder = StateGraph(State).add_sequence([retrieve, generate])
+graph_builder.add_edge(START, "retrieve")
+graph = graph_builder.compile()
+
+response = graph.invoke({"question": "What is Task Decomposition?"})
+print(response["answer"])
