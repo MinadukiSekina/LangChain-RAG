7a7b235b4a084122dd8061618f178837bc93d34b
MinadukiSekina <taroufujimoto075@gmail.com>
2025-08-04 08:17:06 +0000

    refactor: #1 simplify query handling with new agent

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 import bs4
-from langchain import hub
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_core.documents import Document
-from langchain_core.messages import BaseMessage, SystemMessage
 from langchain_core.tools import tool
 from langchain_core.vectorstores import InMemoryVectorStore
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langgraph.graph import END, MessagesState, StateGraph
-from langgraph.prebuilt import ToolNode, tools_condition
-from typing_extensions import TypedDict
+from langgraph.prebuilt import create_react_agent
 
 from src.my_package.environment.env_loader import load_env
 
@@ -38,27 +34,6 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
 # Index chunks
 _ = vector_store.add_documents(documents=all_splits)
 
-# Define prompt for question-answering
-# N.B. for non-US LangSmith endpoints, you may need to specify
-# api_url="https://api.smith.langchain.com" in hub.pull.
-prompt = hub.pull("rlm/rag-prompt")
-
-
-# Define state for application
-class State(TypedDict):
-    question: str
-    context: list[Document]
-    answer: str
-
-
-# Step 1: Generate an AIMessage that may include a tool-call to be sent.
-def query_or_respond(state: MessagesState) -> dict[str, list[BaseMessage]]:
-    """Generate tool call for retrieval or respond."""
-    llm_with_tools = llm.bind_tools([retrieve])
-    response = llm_with_tools.invoke(state["messages"])
-    # MessagesState appends messages to state instead of overwriting
-    return {"messages": [response]}
-
 
 # Define application steps
 @tool(response_format="content_and_artifact")
@@ -71,66 +46,18 @@ def retrieve(query: str) -> tuple[str, list[Document]]:
     return serialized, retrieved_docs
 
 
-# Step 2: Execute the retrieval.
-tools = ToolNode([retrieve])
+agent_executor = create_react_agent(llm, [retrieve])
 
+config = {"configurable": {"thread_id": "def234"}}
 
-# Step 3: Generate a response using the retrieved content.
-def generate(state: MessagesState) -> dict[str, list[BaseMessage]]:
-    """Generate answer."""
-    # Get generated ToolMessages
-    recent_tool_messages = []
-    for message in reversed(state["messages"]):
-        if message.type == "tool":
-            recent_tool_messages.append(message)
-        else:
-            break
-    tool_messages = recent_tool_messages[::-1]
-
-    # Format into prompt
-    docs_content = "\n\n".join(doc.content for doc in tool_messages)
-    system_message_content = (
-        "You are an assistant for question-answering tasks. "
-        "Use the following pieces of retrieved context to answer "
-        "the question. If you don't know the answer, say that you "
-        "don't know. Use three sentences maximum and keep the "
-        "answer concise."
-        "\n\n"
-        f"{docs_content}"
-    )
-    conversation_messages = [
-        message
-        for message in state["messages"]
-        if message.type in ("human", "system") or (message.type == "ai" and not message.tool_calls)
-    ]
-    prompt = [SystemMessage(system_message_content), *conversation_messages]
-
-    # Run
-    response = llm.invoke(prompt)
-    return {"messages": [response]}
-
-
-# Compile application and test
-graph_builder = StateGraph(MessagesState)
-graph_builder.add_node(query_or_respond)
-graph_builder.add_node(tools)
-graph_builder.add_node(generate)
-
-graph_builder.set_entry_point("query_or_respond")
-graph_builder.add_conditional_edges(
-    "query_or_respond",
-    tools_condition,
-    {END: END, "tools": "tools"},
+input_message = (
+    "What is the standard method for Task Decomposition?\n\n"
+    "Once you get the answer, look up common extensions of that method."
 )
-graph_builder.add_edge("tools", "generate")
-graph_builder.add_edge("generate", END)
 
-graph = graph_builder.compile()
-
-input_message = "Hello"
-
-for step in graph.stream(
+for event in agent_executor.stream(
     {"messages": [{"role": "user", "content": input_message}]},
     stream_mode="values",
+    config=config,
 ):
-    step["messages"][-1].pretty_print()
+    event["messages"][-1].pretty_print()
